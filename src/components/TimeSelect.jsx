e29d2100f557355238e5eb502827cc775db6030f
Leonardo <leonardopinheirosilva16@gmail.com>
2026-04-13 07:41:15 -0300

    fix: time ref in TimeSelect component

--- src/components/TimeSelect.jsx
+++ src/components/TimeSelect.jsx
@@ -1,13 +1,16 @@
+import { forwardRef } from "react";
+
 import InputLabel from "./InputLabel";
 
-const TimeSelect = (props) => {
+const TimeSelect = forwardRef((props, ref) => {
   return (
     <div className="flex flex-col gap-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
       <select
-        className="outline-brand-primary placeholder:text-brand-text-gray border-brand-border rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
+        className="rounded-lg border border-solid border-brand-border px-4 py-3 outline-brand-primary placeholder:text-sm placeholder:text-brand-text-gray"
         id="time"
         {...props}
+        ref={ref}
       >
         <option value="morning">Manhã</option>
         <option value="afternoon">Tarde</option>
@@ -15,6 +18,8 @@ const TimeSelect = (props) => {
       </select>
     </div>
   );
-};
+});
+
+TimeSelect.displayName = "TimeSelect";
 
 export default TimeSelect;
