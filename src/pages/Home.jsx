a2a3a82570d475a7a0d36163c0daad30102a8718
Leonardo <leonardopinheirosilva16@gmail.com>
2026-04-21 17:18:20 -0300

    feat: add DashboardCards component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,42 @@
-import { GlassWater, LoaderIcon, TasksIcon, TasksIcon2 } from "../assets/icons";
-import Card from "../components/Card";
+import { Link } from "react-router-dom";
+
+import DashboardCards from "../components/DashboardCards";
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import TaskItem from "../components/TaskItem";
 import { useGetTasks } from "../hooks/data/use-get-tasks";
 
 const HomePage = () => {
-  // hook para chamar a API
   const { data: tasks } = useGetTasks();
-
-  // filtra as tarefas em andamento e as tarefas concluídas
-  const inProgressTask = tasks?.filter(
-    (task) => task.status == "in_progress"
-  ).length;
-  const doneTasks = tasks?.filter((task) => task.status == "done").length;
 
   return (
     <div className="flex">
       <Sidebar />
       <div className="w-full space-y-6 px-8 py-16">
         <Header title="Início" subtitle="Início" />
-        <div className="grid grid-cols-4 gap-9">
-          <Card
-            icon={<TasksIcon2 />}
-            title={tasks?.length}
-            subtitle="Tarefas disponíveis"
-          />
-          <Card
-            icon={<TasksIcon />}
-            title={doneTasks}
-            subtitle="Tarefas concluídas"
-          />
-          <Card
-            icon={<LoaderIcon />}
-            title={inProgressTask}
-            subtitle="Tarefas em andamento"
-          />
-          <Card icon={<GlassWater />} title="40%" subtitle="Água" />
+        <DashboardCards />
+        <div className="grid grid-cols-[2fr,1fr] gap-6">
+          <div className="space-y-6 rounded-[10px] bg-white p-6">
+            <div>
+              <Link to="/tasks" className="text-xl font-semibold">
+                Tarefas
+              </Link>
+              <p className="text-sm text-brand-text-gray">
+                Resumo das tarefas disponíveis
+              </p>
+            </div>
+            <div className="space-y-3">
+              {tasks?.map((task) => (
+                <TaskItem key={task.id} task={task} />
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center justify-center space-y-6 rounded-[10px] bg-white p-6">
+            <p className="text-brand-text-gray">
+              &quot; Cada pequena ação de hoje te aproxima das grandes
+              conquistas de amanhã. Faça o que precisa ser feito! &quot;
+            </p>
+          </div>
         </div>
       </div>
     </div>
